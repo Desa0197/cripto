004f034b05da3c36b8ae013782e0b82bfc11f86f
Denis Surop <denis_surop0197@mail.ru>
2022-04-03 13:58:30 +1000

    Func addition and magic method for BitString

--- lab5/main.py
+++ lab5/main.py
@@ -3,46 +3,104 @@ import json
 
 class BitString:
     def __init__(self, value: str):
-        self._bits = value
+        self.bits = value
 
     def __add__(self, other):
-        if isinstance(other, BitString) and len(self._bits) == len(other._bits):
+        if isinstance(other, BitString) and len(self.bits) == len(other.bits):
             new_bits = ''
 
-            length = len(self._bits)
+            length = len(self.bits)
             for i in range(length):
-                new_bits += str((int(self._bits[i]) + int(other._bits[i])) % 2)
+                new_bits += str((int(self.bits[i]) + int(other.bits[i])) % 2)
 
             return BitString(new_bits)
 
     def __mul__(self, other):
-        if isinstance(other, BitString) and len(self._bits) == len(other._bits):
+        if isinstance(other, BitString) and len(self.bits) == len(other.bits):
             new_bits = ''
 
-            length = len(self._bits)
+            length = len(self.bits)
             for i in range(length):
-                new_bits += str(int(self._bits[i]) * int(other._bits[i]))
+                new_bits += str(int(self.bits[i]) * int(other.bits[i]))
 
             return BitString(new_bits)
+
+    def __floordiv__(self, other):
+        if isinstance(other, BitString) and len(self.bits) == len(other.bits):
+            return self.bits + other.bits
+
+    def __len__(self):
+        return len(self.bits)
 
     def no(self):
         new_bits = ''
 
-        length = len(self._bits)
+        length = len(self.bits)
         for i in range(length):
-            new_bits += str(1 - int(self._bits[i]))
+            new_bits += str(1 - int(self.bits[i]))
 
         return BitString(new_bits)
 
     def csr(self, steps: int):
-        new_bits = self._bits[-steps:] + self._bits[:-steps]
+        new_bits = self.bits[-steps:] + self.bits[:-steps]
 
         return BitString(new_bits)
 
     def sr(self, steps: int):
-        new_bits = '0' * steps + self._bits[:-steps]
+        new_bits = '0' * steps + self.bits[:-steps]
 
         return BitString(new_bits)
+
+    def split(self, length):
+        list_blocks = []
+
+        for i in range(0, len(self.bits), length):
+            list_blocks.append(self.bits[i:i + length])
+
+        return list_blocks
+
+    def mod_f_b_in_decimal(self) -> int:
+        num = 0
+        for i in range(len(self.bits)):
+            num += int(self.bits[i]) * 2 ** (len(self.bits) - i - 1)
+        return num
+
+    @staticmethod
+    def mod_f_d_in_bit(num: int, length):
+        if num == 0 and length != 0:
+            return '0' * length
+        elif num == 0:
+            return '0'
+
+        rev_bit_num = []
+        while num != 0:
+            m = num % 2
+            num //= 2
+            rev_bit_num.append(str(m))
+
+        zero_list = ['0' for _ in range(length - len(rev_bit_num))]
+        rev_bit_num += zero_list
+
+        if length != 0:
+            rev_bit_num = rev_bit_num[:length]
+
+        rev_bit_num.reverse()
+        bit_num = ''.join(rev_bit_num)
+        return BitString(bit_num)
+
+
+def addition_message(mes: BitString) -> BitString:
+    length = len(mes)
+    k = (448 - length - 1) % 512
+
+    addition = BitString('1' + '0' * k) // BitString.mod_f_d_in_bit(length, 64)
+    new_mes = mes // addition
+
+    return new_mes
+
+
+def split_message(mes: BitString) -> list[list[BitString]]:
+    pass
 
 
 def ch(x: BitString, y: BitString, z: BitString) -> BitString:
@@ -72,5 +130,3 @@ def sigma1(x: BitString) -> BitString:
 if __name__ == '__main__':
     # ch('10101', '01110', '')
     pass
-
-
